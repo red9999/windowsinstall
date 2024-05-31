f281a5b8a05b465eb922467594e90e4032db4e2e
red9999 <47200164+red9999@users.noreply.github.com>
2024-06-01 07:57:13 +0900

    windows taskbar

--- winstall-1817.ps1
+++ winstall-1817.ps1
@@ -1,1 +1,8 @@
 winget install --id=Microsoft.VisualStudioCode -e  ; winget install --id=7zip.7zip -e  ; winget install --id=WireGuard.WireGuard -e  ; winget install --id=Brave.Brave -e  ; winget install --id=voidtools.Everything -e  ; winget install --id=Flow-Launcher.Flow-Launcher -e  ; winget install --id=JetBrains.PyCharm.Community -e  ; winget install --id=VideoLAN.VLC -e  ; winget install --id=Adobe.Acrobat.Reader.64-bit -e  ; winget install --id=qBittorrent.qBittorrent -e  ; winget install --id=calibre.calibre -e 
+
+# Set the TaskbarGlomLevel value to 2 to never combine taskbar buttons
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarGlomLevel" -Value 2
+
+# Restart Explorer to apply the changes
+Stop-Process -Name explorer -Force
+Start-Process explorer
