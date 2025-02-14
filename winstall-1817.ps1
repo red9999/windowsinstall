b234bdf5de2b7db437c0a43e6ff78efc84eaf288
red9999 <47200164+red9999@users.noreply.github.com>
2025-02-14 15:15:03 +0900

    Update winstall-1817.ps1

--- winstall-1817.ps1
+++ winstall-1817.ps1
@@ -1,4 +1,95 @@
-winget install --id=Microsoft.VisualStudioCode -e  ; winget install --id=7zip.7zip -e  ; winget install --id=WireGuard.WireGuard -e  ; winget install --id=Brave.Brave -e  ; winget install --id=voidtools.Everything -e  ; winget install --id=Flow-Launcher.Flow-Launcher -e  ; winget install --id=JetBrains.PyCharm.Community -e  ; winget install --id=VideoLAN.VLC -e  ; winget install --id=Adobe.Acrobat.Reader.64-bit -e  ; winget install --id=qBittorrent.qBittorrent -e  ; winget install --id=calibre.calibre -e ; winget install --id=Kakao.KakaoTalk  -e ; winget install -e --id Mozilla.Firefox
+#also run https://github.com/jjpferreira/win11debloat
+
+
+# Development Tools
+winget install --id=Microsoft.VisualStudioCode -e  
+# winget install --id=JetBrains.PyCharm.Community -e  
+
+# Utilities
+winget install --id=7zip.7zip -e  
+# winget install --id=WireGuard.WireGuard -e  
+winget install --id=VideoLAN.VLC -e  
+# winget install --id=Adobe.Acrobat.Reader.64-bit -e  
+# winget install --id=calibre.calibre -e  
+winget install --id=voidtools.Everything -e  
+winget install --id=Flow-Launcher.Flow-Launcher -e  
+# winget install --id=qBittorrent.qBittorrent -e  
+# winget install --id=Kakao.KakaoTalk -e  
+winget install --id=SumatraPDF.SumatraPDF -e  # Install Sumatra PDF
+
+# Communication Tools
+winget install --id=Mozilla.Firefox -e  # Install Firefox
+winget install --id=Thunderbird.Thunderbird -e  # Install Thunderbird Email
+winget install --id=Postman.Postman -e  # Install Postman
+winget install --id=PuTTY.PuTTY -e  # Install PuTTY
+
+# Docker and Virtualization
+winget install --id=Docker.DockerDesktop -e  # Install Docker Desktop
+
+# Remove Windows Apps
+Get-AppxPackage *YourPhone* | Remove-AppxPackage
+Get-AppxPackage -allusers *YourPhone* | Remove-AppxPackage
+Get-AppxProvisionedPackage -online | where-object {$_.packagename -like "*YourPhone*"} | Remove-AppxProvisionedPackage -online
+
+# "Disable AutoUpdate Apps Microsoft Store"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f
+# "Block the automatic installation of suggested Windows 10 apps"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /t REG_DWORD /d 0 /f
+# "Disable Showing App Suggestions in Start in Windows 10 (settings app)"
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338388Enabled /t REG_DWORD /d 0 /f
+# "Disable OEM Apps"
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v OemPreInstalledAppsEnabled /t REG_DWORD /d 0 /f
+# "Disable Promotional Apps"
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v PreInstalledAppsEnabled /t REG_DWORD /d 0 /f
+pause
+
+# "Disable Cloud Search"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCloudSearch /t REG_DWORD /d 0 /f
+# "Disable Cortana"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
+# "Disable Cortana LockScreen"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortanaAboveLock /t REG_DWORD /d 0 /f
+# "Disable Cortana"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v CortanaEnabled /t REG_DWORD /d 0 /f
+# "Disable Cortana"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /t REG_DWORD /d 0 /f
+pause
+
+# Restore The Explorer Ribbon
+reg delete "HKCU\Software\Classes\CLSID\{d93ed569-3b3e-4bff-8355-3c44f6a52bb5}" /f
+taskkill /F /IM explorer.exe
+start explorer.exe
+
+# Dark Theme
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f 
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 1 /f
+
+# Force File Explorer to open to This PC instead of Quick Access
+echo 3D Objects
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Videos
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{35286a68-3c57-41a1-bbb1-0eae73d76c95}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{35286a68-3c57-41a1-bbb1-0eae73d76c95}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Documents
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{f42ee2d3-909f-4907-8871-4c22fc0bf756}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{f42ee2d3-909f-4907-8871-4c22fc0bf756}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Downloads
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d83ee9b-2244-4e70-b1f5-5393042af1e4}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d83ee9b-2244-4e70-b1f5-5393042af1e4}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Images
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{0ddd015d-b06c-45d5-8c4c-f59713854639}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{0ddd015d-b06c-45d5-8c4c-f59713854639}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Music
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{a0c69a99-21c8-4671-8703-7934162fcf1d}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{a0c69a99-21c8-4671-8703-7934162fcf1d}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+echo Desktop
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}\PropertyBag" /v ThisPCPolicy /t REG_SZ /d Hide /f
+taskkill /F /IM explorer.exe
+start explorer.exe
+pause
 
 # Set the TaskbarGlomLevel value to 2 to never combine taskbar buttons
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarGlomLevel" -Value 2
@@ -6,8 +97,8 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 # Set old context menu
 New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Value "" -Force
 
-#Remove all pinned apps from start menu
-#Requires -RunAsAdministrator
+# Remove all pinned apps from the start menu
+# Requires -RunAsAdministrator
 
 $START_MENU_LAYOUT = @"
 <LayoutModificationTemplate xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout" xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout" Version="1" xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout" xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification">
@@ -22,18 +113,18 @@ $START_MENU_LAYOUT = @"
 
 $layoutFile="C:\Windows\StartMenuLayout.xml"
 
-#Delete layout file if it already exists
+# Delete layout file if it already exists
 If(Test-Path $layoutFile)
 {
     Remove-Item $layoutFile
 }
 
-#Creates the blank layout file
+# Creates the blank layout file
 $START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
 
 $regAliases = @("HKLM", "HKCU")
 
-#Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
+# Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
 foreach ($regAlias in $regAliases){
     $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
     $keyPath = $basePath + "\Explorer" 
@@ -44,25 +135,21 @@ foreach ($regAlias in $regAliases){
     Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
 }
 
-#Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
+# Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
 Stop-Process -name explorer
 Start-Sleep -s 5
 $wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
 Start-Sleep -s 5
 
-#Enable the ability to pin items again by disabling "LockedStartLayout"
+# Enable the ability to pin items again by disabling "LockedStartLayout"
 foreach ($regAlias in $regAliases){
     $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
     $keyPath = $basePath + "\Explorer" 
     Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
 }
 
-
 Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
 Remove-Item $layoutFile
-
-
-
 
 # Restart Explorer to apply the changes
 Stop-Process -Name explorer -Force
